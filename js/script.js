be8e5f07043be51b184707a0e9ea0a7457e4aa55
gibstock <gibstock@gmail.com>
2020-01-03 01:18:43 -0800

    footer

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const img = document.querySelectorAll('img');
 const banner = document.querySelector('.banner');
 const logo = banner.querySelector('h3');
+const artistMenu = document.querySelector('.floating-artist');
+const artBtn = artistMenu.querySelector('h1');
+const menuUl = artistMenu.querySelector('ul');
+
 
 
 function toggleMenu(){
@@ -17,11 +21,20 @@ function toggleMenu(){
 function goHome(){
     location.assign('https://www.onk.gallery');
 }
+function artListMenu(){
+    if(menuUl.style.display === '' || menuUl.style.display === 'none'){
+        menuUl.style.display = 'inherit';
+    }else if(menuUl.style.display === 'inherit'){
+        menuUl.style.display = 'none';
+    }
+}
+
 // function picLink(e){
 //     alert('hi');
 // }
 
 sideNav.addEventListener('click', toggleMenu);
 logo.addEventListener('click', goHome);
+artBtn.addEventListener('click', artListMenu);
 // picture.addEventListener('click', picLink);
 
