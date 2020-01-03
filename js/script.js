5aa512c0bc0f8abb662d33c1c887b88d98bd0486
gibstock <gibstock@gmail.com>
2020-01-03 03:26:31 -0800

    js dance

--- js/script.js
+++ js/script.js
@@ -3,11 +3,23 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const img = document.querySelectorAll('img');
 const banner = document.querySelector('.banner');
 const logo = banner.querySelector('h3');
-if(document.querySelector('.floating-artist') !== null){
+document.addEventListener('DOMContentLoaded', function(){
     const artistMenu = document.querySelector('.floating-artist');
-    const artBtn = artistMenu.querySelector('h1');
-    const menuUl = artistMenu.querySelector('ul');
-}
+    if(artistMenu){
+        const artBtn = artistMenu.querySelector('h1');
+        const menuUl = artistMenu.querySelector('ul');
+        artBtn.addEventListener('click', artListMenu);
+        function artListMenu(){
+            if(menuUl.style.display === '' || menuUl.style.display === 'none'){
+                menuUl.style.display = 'inherit';
+            }else if(menuUl.style.display === 'inherit'){
+                menuUl.style.display = 'none';
+            }
+        }
+    }
+})
+
+
 
 
 function goHome(){
@@ -23,13 +35,7 @@ function toggleMenu(){
     }
 }
 
-function artListMenu(){
-    if(menuUl.style.display === '' || menuUl.style.display === 'none'){
-        menuUl.style.display = 'inherit';
-    }else if(menuUl.style.display === 'inherit'){
-        menuUl.style.display = 'none';
-    }
-}
+
 
 // function picLink(e){
 //     alert('hi');
@@ -37,6 +43,6 @@ function artListMenu(){
 
 sideNav.addEventListener('click', toggleMenu);
 logo.addEventListener('click', goHome);
-artBtn.addEventListener('click', artListMenu);
+
 // picture.addEventListener('click', picLink);
 
